0caf6724ce792a99f818c157911bab8c2dbda85c
Aaron <aaronwang@rancher.com>
2017-06-09 12:15:30 +0800

    Polish pipeline detail page

--- app/components/pipeline-steps/component.js
+++ app/components/pipeline-steps/component.js
@@ -3,11 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   sortFinishText: null,
   model: null,
+  sortingScope: function(){
+    return this.get('stageId')
+  }.property('stageId'),
   actions: {
     dragStart: function(object) {
       console.log('Drag Start', object);
     },
     sortEndAction: function() {
+      console.log(this.get('stageId'));
       console.log('Sort Ended', this.get('model.pages'));
     }
   }
--- app/pipeline/controller.js
+++ app/pipeline/controller.js
@@ -3,71 +3,30 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   model: function(){
     return this.get('model')
-  }.property(),
-  activeTab: 'history',
-  isActivity: function(){
-      return this.get('activeTab') === 'activity'
-  }.property(),
+  }.property('model'),
+
+  filterdPiplineHistory: function(){
+    return this.get('model').pipeline.activities;
+  }.property('activeTab'),
+  // TODO, when no Activity， default to History
+  activeTab: 'activity',
   isHistory: function(){
     return this.get('activeTab') === 'history'
-  }.property(),
+  }.property('activeTab'),
+
+  stageStatusClass: function(){
+    return 
+  }.property('model.@'),
+
+  actions:{
+    setActiveTab: function(tab){
+      this.set('activeTab',tab)
+    }
+  },
   
   init() {
     // var model = this.get('model')
     // console.log(model)
     // this.set('pipelineName',model.name)
   }
-
 });
-
-// import Ember from 'ember';
-// import { tagsToArray } from 'ui/models/stack';
-
-// export default Ember.Controller.extend({
-//   projectController: Ember.inject.controller('authenticated.project'),
-//   projects: Ember.inject.service(),
-//   prefs: Ember.inject.service(),
-//   intl: Ember.inject.service(),
-//   activeTab: 'history',
-//   isActivity: function(){
-//       return this.get('activeTab') === 'activity'
-//   }.property(),
-//   isHistory: function(){
-//     return this.get('activeTab') === 'history'
-//   }.property(),
-//   tags: Ember.computed.alias('projectController.tags'),
-//   sortBy: 'name',
-
-//   expandedStacks: null,
-
-//   init() {
-//     this._super(...arguments);
-//     this.set('expandedStacks',[]);
-//   },
-
-//   actions: {
-//     toggleExpand(instId) {
-//       let list = this.get('expandedStacks');
-//       if ( list.includes(instId) ) {
-//         list.removeObject(instId);
-//       } else {
-//         list.addObject(instId);
-//       }
-//     },
-//   },
-
-//   filteredStacks: function() {
-//     var needTags = tagsToArray(this.get('tags'));
-//     var out = this.get('model.stacks').filterBy('isFromCatalog', true);
-
-//     if ( !this.get('prefs.showSystemResources') ) {
-//       out = out.filterBy('system', false);
-//     }
-
-//     if ( needTags.length ) {
-//       out = out.filter((obj) => obj.hasTags(needTags));
-//     }
-
-//     return out;
-//   }.property('model.stacks.@each.{type,isFromCatalog,tags}','tags','prefs.showSystemResources'),
-// });
