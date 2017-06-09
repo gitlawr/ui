05b6c52eba9577a4957fe64aa2e21ca6f0fbd798
Aaron <aaronwang@rancher.com>
2017-06-09 12:15:30 +0800

    优化pipeline详情页

--- app/components/pipeline-history-table/component.js
+++ app/components/pipeline-history-table/component.js
@@ -1,32 +1,5 @@
 import Ember from 'ember';
 
-export const headersAll =  [
-  {
-    name: 'name',
-    sort: ['name'],
-    searchField: 'name',
-    translationKey: 'generic.name',
-  },
-  {
-    name: 'status',
-    sort: ['name'],
-    searchField: 'status',
-    translationKey: 'generic.state',
-  },
-  {
-    name: 'status',
-    sort: ['name'],
-    searchField: 'status',
-    translationKey: 'generic.message',
-  },
-  {
-    name: 'status',
-    sort: ['name'],
-    searchField: 'status',
-    translationKey: 'generic.timeStamp',
-  },
-];
-
 export default Ember.Component.extend({
   prefs: Ember.inject.service(),
 
@@ -34,10 +7,6 @@ export default Ember.Component.extend({
 
   sortBy: 'name',
   body: null,
-  headers: function() {
-    return headersAll;
-  }.property(),
-
   filtered: function() {
     let out = this.get('body')||[];
     return out;
