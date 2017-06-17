54ea43a684f2948b01bea58f44ba083071371a55
Aaron <aaronwang@rancher.com>
2017-06-17 09:57:29 +0800

    bug fix

--- app/pipeline/controller.js
+++ app/pipeline/controller.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  model: function() {
-    return this.get('model')
-  }.property('model'),
+  model: null,
   activeTab: 'history',
   filters: {
     activity: ['Waitting', 'Running'],
@@ -11,13 +9,17 @@ export default Ember.Controller.extend({
   },
   init() {
     this._super(...arguments);
-    var activities = this.get('model').activities.filter(ele => {
-      return this.get('filters')['activity'].indexOf(ele.status) !== -1
-    })
-    if (activities) {
-      this.set('activeTab', 'activity');
-    }
   },
+  initTab: function() {
+    Ember.run.once(() => {
+      var activities = this.get('model').activities.filter(ele => {
+        return this.get('filters')['activity'].indexOf(ele.status) !== -1
+      });
+      if (activities) {
+        this.set('activeTab', 'activity');
+      }
+    });
+  }.observes('model'),
   filterdPiplineHistory: function() {
     var tab = this.get('activeTab');
     return this.get('model').activities.filter(ele => {
