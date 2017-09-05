b73f035eaa6724b956a1ee55abbf2a490e85ebb1
Aaron <aaronwang@rancher.com>
2017-09-05 19:26:12 +0800

    Set Default EntryPoint Type Of Task

--- app/components/pipeline/pipeline-step-task/component.js
+++ app/components/pipeline/pipeline-step-task/component.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  isShellObserves: function(){
+    var isService = this.get('selectedModel.isService');
+    if(isService){
+      this.set('selectedModel.isShell',false);
+    }else{
+      this.set('selectedModel.isShell',true);
+    }
+  }.observes('selectedModel.isService'),
   actions: {
     changeTaskModel(state){
       this.set('selectedModel.isShell',state);
